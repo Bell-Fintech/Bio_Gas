4fa5787d5f136ed6da364aca4dcbd25742ddb8b3
Alexpan <740583913@qq.com>
2022-11-04 14:16:24 +0800

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import leafmap.foliumap as leafmap
+#import leafmap.foliumap as leafmap
 
 st.set_page_config(layout="wide")
 
@@ -40,6 +40,6 @@ markdown = """
 
 st.markdown(markdown)
 
-m = leafmap.Map(minimap_control=True)
-m.add_basemap("OpenTopoMap")
-m.to_streamlit(height=500)
+# m = leafmap.Map(minimap_control=True)
+# m.add_basemap("OpenTopoMap")
+# m.to_streamlit(height=500)
